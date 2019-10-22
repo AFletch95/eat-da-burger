b30cfeddddac7fb7183e3e65ee708f87f5fea5ab
Afletch <austinj.fletcher@gmail.com>
2019-10-22 14:31:26 -0700

    database info

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ('Guacamole Bacon');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Double Cheese');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Hamburger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Imposible Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Whisky BBQ');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Hawaiian');
